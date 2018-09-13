ade3ebcb903a36d7fa483aa78f69571060a65adf
Marcos <mp2772@gmail.com>
2018-09-12 22:55:38 -0400

    completed spotify lab

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -3,32 +3,39 @@ const app = express();
 const hbs = require('hbs');
 const SpotifyWebApi = require('spotify-web-api-node');
 
+require("dotenv").config();
 // Remember to paste here your credentials
-const clientId = '1c30624cba6742dcb792991caecae571',
-    clientSecret = '746977b1e77240faa9d0d2411c3e0efe';
+const clientId = process.env.SPOTIFY_ID,
+    clientSecret = process.env.SPOTIFY_SECRET;
 
 const spotifyApi = new SpotifyWebApi({
   clientId : clientId,
   clientSecret : clientSecret
 });
 
+app.use(express.static('public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+
 // Retrieve an access token.
 spotifyApi.clientCredentialsGrant()
-  .then(function(data) {
+  .then((data) => {
     spotifyApi.setAccessToken(data.body['access_token']);
-  }, function(err) {
+  })
+  .catch((err) => {
     console.log('Something went wrong when retrieving an access token', err);
 });
 
 
 app.get('/', (req, res, next) => {
-  res.render('views/index');
+  res.render('index');
 });
 
 app.get('/artists', (req, res, next) => {
   spotifyApi.searchArtists(req.query.artistInput)
-    .then(data => {
-      res.render();
+    .then(result => {
+      console.log(result.body.artists.items);
+      res.render('artists', {data: result.body.artists.items});
     })
     .catch(err => {
       next(err);
@@ -37,11 +44,29 @@ app.get('/artists', (req, res, next) => {
 
 
 app.get('/albums/:artistId', (req, res, next) => {
-  spotifyApi.getArtistAlbums('req.params.artistId')
-  .then((data) => {
-    res.render('views/albums', data.body);
+  spotifyApi.getArtistAlbums(req.params.artistId)
+  .then((result) => {
+    console.log("the albums info ==================== ", result.body.items);
+    res.render('albums', {data: result.body.items});
   })
   .catch((err) => {
     next(err);
   });
 });
+
+
+app.get('/tracks/:albumId', (req, res, next) => {
+  spotifyApi.getAlbumTracks(req.params.albumId)
+  .then((result) => {
+    console.log("track info ------------------------ ", result.body.items[0]);
+    res.render('tracks', {data: result.body.items});
+  })
+  .catch((err) => {
+    next(err);
+  });
+});
+
+
+app.listen(3000);
+
+module.exports = app;
